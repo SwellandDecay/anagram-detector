e7603818c2c89160c7771b9d6a0a1c12c3834e97
Garrett <Garrett.Bodley@gmail.com>
2020-10-29 13:58:00 -0400

    commit tree c2cb7a233db33c9b4a3cb3683bd37fb620019572

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    attr_accessor :reference, :words
+
+    def initialize(reference=nil)
+        @reference = reference
+    end
+
+    def match(words=nil)
+        @words = words
+        reference = @reference
+        anagrams = []
+
+        words.each do |word|
+            if word.split("").sort == reference.split("").sort
+                anagrams << word
+            end
+        end
+        anagrams
+    end
+
+
+
+end
